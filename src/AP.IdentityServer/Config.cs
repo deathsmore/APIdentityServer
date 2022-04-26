54e41ca63e8f200ff92105ee59d2ff2ee9f481f1
tuanna2 <tuanna2@daivietgroup.net.vn>
2022-04-26 14:43:25 +0700

    Init project

---
+++ src/AP.IdentityServer/Config.cs
@@ -0,0 +1,91 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace NewCore.IDP
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("DVG.AutoPortal.CMS.CarInfo", "DVG AutoPortal CMS CarInfo")
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new List<ApiResource>
+            {
+                new ApiResource("DVG.AutoPortal.CMS.CarInfo", "DVG AutoPortal CMS CarInfo")
+                {
+                    Scopes = { "DVG.AutoPortal.CMS.CarInfo" }
+                }
+            };
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client()
+                {
+                    ClientName = "DVG AutoPortal CMS Client",
+                    ClientId =  "DVG.AutoPortal.CMS.Spa",
+                    RequireClientSecret = false,
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequirePkce = true,
+                    AllowAccessTokensViaBrowser = true,
+                    RequireConsent = false,
+                    RedirectUris = { 
+                        "http://localhost:4200/signin-callback", 
+                        "http://localhost:4200/assets/silent-callback.html" 
+                    },
+                    PostLogoutRedirectUris = { 
+                        "http://localhost:4200/signout-callback" 
+                    },
+                    AllowedCorsOrigins = { 
+                        "http://localhost:4200" 
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "DVG.AutoPortal.CMS.CarInfo"
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("DVG.AutoPortal.CMS.Spa".Sha256())
+                    },
+                    AccessTokenLifetime = 600
+
+                }
+                //new Client()
+                //{
+                //    ClientName = "Image Gallery",
+                //    ClientId =  "imagegalleryclient",
+                //    AllowedGrantTypes = GrantTypes.Code,
+                //    RedirectUris = new List<string>()
+                //    {
+                //        "https://localhost:44389/signin-oidc"
+                //    },
+                //    AllowedScopes =
+                //    {
+                //        IdentityServerConstants.StandardScopes.OpenId,
+                //        IdentityServerConstants.StandardScopes.Profile
+                //    },
+                //    ClientSecrets =
+                //    {
+                //        new Secret("secret".Sha256())
+                //    }
+
+                //}
+            };
+    }
+}
---
+++ src/AP.IdentityServer/Quickstart/Account/AccountOptions.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using System;
+
+namespace IdentityServerHost.Quickstart.UI
+{
+    public class AccountOptions
+    {
+        public static bool AllowLocalLogin = true;
+        public static bool AllowRememberLogin = true;
+        public static TimeSpan RememberMeLoginDuration = TimeSpan.FromDays(30);
+
+        public static bool ShowLogoutPrompt = false;//Không hiện view thông báo đã logout
+        public static bool AutomaticRedirectAfterSignOut = true;//Tự động redirect về client sau khi IS4 logout
+
+        public static string InvalidCredentialsErrorMessage = "Invalid username or password";
+    }
+}
